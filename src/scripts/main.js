22be59df8ce6b6a0092dc80d300a3fe2db261511
Oles Partyka <oles.partyka.29@gmail.com>
2024-09-29 02:08:14 +0200

    mobile version has been added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 'use strict';
+const body = document.querySelector('body');
+const sidebar = body.querySelector('.sidebar');
+const headerArr = body.querySelector('.header__item-arr');
+
+function toggleCloseClass() {
+  if (sidebar) {
+    if (window.innerWidth <= 970) {
+      sidebar.classList.add('close');
+    } else {
+      sidebar.classList.remove('close');
+    }
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  toggleCloseClass();
+  window.addEventListener('resize', toggleCloseClass);
+});
+
+headerArr.addEventListener('click', () =>{
+  sidebar.classList.toggle('close');
+});
